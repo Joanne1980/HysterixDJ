ef29891cdcc50359e255443d751fa8bf33cca498
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-04 14:21:42 +0000

    Add About model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class About(models.Model):
+    artist = models.CharField(max_length=200,)
+    image = models.ImageField() 
+    content = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
