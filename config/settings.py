d0f433cf00d0e0bcb1cb9a5cda7075c8136729c8
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-04 12:31:35 +0000

    Add cloudinary

--- config/settings.py
+++ config/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     # Other
     'crispy_forms',
     'crispy_bootstrap5'
+    'cloudinary',
+    'cloudinary_storage',
 
 ]
 
@@ -174,6 +176,10 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#Cloudinary Settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
