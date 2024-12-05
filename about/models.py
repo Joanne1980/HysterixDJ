b10686455c99aa29a01e85addc8be0bbc0d50117
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-05 09:18:41 +0000

    Worked on about app

--- about/models.py
+++ about/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
     artist = models.CharField(max_length=200,)
-    image = models.ImageField() 
+    featured_image = CloudinaryField('image', default='placeholder') 
     content = models.TextField()
     slug = models.SlugField(max_length=200, unique=True)
 
-    def __str__(self):
-        return f"{self.title}"
+    
 
 
